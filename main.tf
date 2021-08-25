5d2cc59efa49ce6697611765868eff4abec13928
EL HIMIR ABDERRAHIM <59322515+abdrrahimelh@users.noreply.github.com>
2021-08-25 01:10:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0f7cd40eac2214b37"
   instance_type = "t2.micro"
-  user_data = file("./install_docker_machine_compose.sh")
+  user_data = file("/install_docker_machine_compose.sh")
   key_name="linux-key"
   security_groups = ["swarm_sg"]
   tags = {
